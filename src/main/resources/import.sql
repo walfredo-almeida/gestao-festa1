ca6b081112cc70bbf21b7fed2af83ea6a2d32dc6
walfredo <awalfredo@hotmail.com>
2021-04-28 18:16:27 -0300

    criado profiles

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,2 @@
-insert into convidado (id, nome, acompanhantes) values (1, 'Pedro', 2);
-insert into convidado (id, nome, acompanhantes) values (2, 'Maria', 3);
-insert into convidado (id, nome, acompanhantes) values (3, 'Ricardo', 1);
+insert into convidado (acompanhantes, data_entrada, nome, sobrenome, id) values (2, 2021-04-27, 'Pedro', 'Pedro1', 1);
+
